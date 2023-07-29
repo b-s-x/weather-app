269571bafd5a8f4ab14277e17c44969c8fe36817
b-s-x <b.s.x.mars@gmail.com>
2023-07-29 12:03:22 +0300

    add initial structure

---
+++ src/widgets/weather/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Header from './ui/Header.vue';
+import Main from './ui/Main.vue';
+import Settings from './ui/Settings.vue';
+import Info from './ui/Info.vue';
+
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
