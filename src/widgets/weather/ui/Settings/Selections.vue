71327d9c50709884572887d49fed2899344ddc34
b-s-x <b.s.x.mars@gmail.com>
2023-07-30 12:48:06 +0300

    temp add draggable

--- src/widgets/weather/ui/Settings/Selections.vue
+++ src/widgets/weather/ui/Settings/Selections.vue
@@ -1,36 +1,58 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import draggable from 'vuedraggable';
 import { SelectedCity } from '@/widgets/weather/model/types';
 import Item from './Item.vue';
 
-interface Props {
-  selected: SelectedCity[]
-}
-
-defineProps<Props>()
+const props = defineProps();
+const selected = ref<SelectedCity[]>([
+  {
+    id: 1,
+    city: 'fff',
+    country: 'GE',
+  },
+  {
+    id: 2,
+    city: 'aaa',
+    country: 'GE',
+  },
+]);
 
 const handleDelete = (value: any) => console.log(222, value);
+const dragOptions = { animation: 200 };
 
+const updateSelected = (newSelected: SelectedCity[]) => {
+  selected.value = newSelected;
+};
 </script>
 
 <template>
   <div>
-    <div
-      v-for="({ id, city, country }) of selected"
-      :key="id"
+    <draggable
+      v-model="selected"
+      :options="dragOptions"
+      itemKey="drag"
+      @update:modelValue="updateSelected"
     >
-      <Item
-        class="item"
-        :id="id"
-        :city="city"
-        :country="country"
-        @deleteSelected="handleDelete"
-      />
-    </div>
+      <template
+        v-for="({ id, city, country }, index) of selected"
+        :key="id" v-slot:item="{ element }"
+      >
+        <div :key="element.id">
+          <Item
+            class="item"
+            :id="element.id"
+            :city="element.city"
+            :country="element.country"
+            @deleteSelected="handleDelete"
+          />
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 .item {
   margin-bottom: 8px;
 }
