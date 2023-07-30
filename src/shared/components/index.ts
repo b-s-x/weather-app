d94a6b553eb68c4e205f572b833b7e0308e7dae4
b-s-x <b.s.x.mars@gmail.com>
2023-07-30 22:03:45 +0300

    delete

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,11 +1,9 @@
 import Spinner from './Spinner/index.vue';
 import { SpinnerTypes } from './Spinner/types';
 import SVGContainer from './SVGContainer.vue';
-import Draggable from './Draggable.vue';
 
 export {
   Spinner,
   SVGContainer,
   SpinnerTypes,
-  Draggable,
 }
